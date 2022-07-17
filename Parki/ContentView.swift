5870c791fb9d6fd086e46b8a6c4447b75422bddc
Yi Qin <yqin93@gatech.edu>
2022-07-17 12:31:58 -0400

    Initial Commit

---
+++ Parki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parki
+//
+//  Created by Tianyi Chen on 7/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Parki/ParkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkiApp.swift
+//  Parki
+//
+//  Created by Tianyi Chen on 7/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ParkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
